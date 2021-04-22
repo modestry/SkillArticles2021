520c29fdc8d4382d998ceb08a59a79c1eb51cd4f
p.petrov <modestry2007@yandex.ru>
2021-04-22 14:53:40 +0300

    hometask_6

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -1,13 +1,14 @@
 package ru.skillbranch.skillarticles.extensions
 
-import android.content.Context
+import android.os.Parcelable
+import android.util.SparseArray
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
 
 fun View.setMarginOptionally(left: Int? = null, top: Int? = null,
                              right: Int? = null, bottom: Int? = null)
 {
-    val viewContext: Context = this.context
     layoutParams<ViewGroup.MarginLayoutParams> {
         left?.let { leftMargin = it }
         top?.let { topMargin =  it }
@@ -16,6 +17,25 @@ fun View.setMarginOptionally(left: Int? = null, top: Int? = null,
     }
 }
 
+fun View.setPaddingOptionally(left: Int = paddingLeft, top: Int = paddingTop,
+                             right: Int = paddingRight, bottom: Int = paddingBottom)
+{
+    setPadding(left , top , right, bottom)
+}
+
 inline fun <reified T : ViewGroup.LayoutParams> View.layoutParams(block: T.() -> Unit) {
     if (layoutParams is T) block(layoutParams as T)
+}
+
+inline fun <reified T: View> View.idTag(idx: Int): String = "cv${T::class.java.simpleName}_${idx}"
+
+
+fun ViewGroup.saveChildViewStates(): SparseArray<Parcelable> {
+    val childViewStates = SparseArray<Parcelable>()
+    children.forEach { child -> child.saveHierarchyState(childViewStates) }
+    return childViewStates
+}
+
+fun ViewGroup.restoreChildViewStates(childViewStates: SparseArray<Parcelable>) {
+    children.forEach { child -> child.restoreHierarchyState(childViewStates) }
 }
